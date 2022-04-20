48e9ffd523e1cb457ce28b99171769b102b6b35d
Megan Brant <mbrant1@butlercc.edu>
2022-04-19 21:54:58 -0500

    intersting???

---
+++ Assets/hm.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //idk what im doing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
